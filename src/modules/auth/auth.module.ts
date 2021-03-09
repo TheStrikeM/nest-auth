1bf1dc94f464b65f05f7052ddba6313c7e65d7e2
Danil <dinkodanil@gmail.com>
2021-03-09 11:59:59 +0300

    Connect this controller to auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import UserModule from '../user/user.module';
 import AuthService from './services/auth.service';
 import LocalStrategy from './services/local.strategy';
+import AuthController from './controller/auth.controller';
 
 
 @Module({
   imports: [UserModule],
-  controllers: [],
+  controllers: [AuthController],
   providers: [AuthService, LocalStrategy]
 })
 export default class AuthModule {}
