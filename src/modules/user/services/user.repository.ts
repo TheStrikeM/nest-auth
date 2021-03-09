98bd84803e65eea4414c57867d13cdab53e1eba6
Danil <dinkodanil@gmail.com>
2021-03-09 11:32:27 +0300

    Setting user repository

---
+++ src/modules/user/services/user.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from '../schema/User';
+
+
+@Injectable()
+export default class UserRepository {
+
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>
+  ) {}
+
+  async
+
+}
