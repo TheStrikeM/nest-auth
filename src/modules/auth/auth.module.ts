11e52dd64e20775226c983ffbcac28a02d6ed707
Danil <dinkodanil@gmail.com>
2021-03-09 12:31:23 +0300

    Connect JWTModule and setting this module in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,11 +3,26 @@ import UserModule from '../user/user.module';
 import AuthService from './services/auth.service';
 import LocalStrategy from './services/local.strategy';
 import AuthController from './controller/auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') }
+      }),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AuthController],
+
   providers: [AuthService, LocalStrategy]
 })
 export default class AuthModule {}
--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import UserRepository from '../../user/services/user.repository';
+import { User } from '../../user/schema/User';
 import UserDto from '../../user/dto/UserDto';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export default class AuthService {
 
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService
+  ) {}
 
   async validateUser(username: string, pass: string): Promise<UserDto> {
-    const candidate: UserDto = await this.userRepository.findByUsername(username)
+    const candidate: User = await this.userRepository.findByUsername(username)
     if(candidate && candidate.password === pass) {
       const {password, ...secureUser} = candidate
       return secureUser
     }
 
     return null
+  }
+
+  async login(user: UserDto) {
+    const payload = { id: user._id, username: user.username }
+    return {
+      accessToken: this.jwtService.sign(payload)
+    }
   }
 }
