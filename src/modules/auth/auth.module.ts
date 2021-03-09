daabfa055a660c97c56f18fcd7b391c2a14c5308
Danil <dinkodanil@gmail.com>
2021-03-09 11:57:44 +0300

    Connect localstrategy to auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import UserModule from '../user/user.module';
 import AuthService from './services/auth.service';
+import LocalStrategy from './services/local.strategy';
 
 
 @Module({
   imports: [UserModule],
   controllers: [],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy]
 })
 export default class AuthModule {}
