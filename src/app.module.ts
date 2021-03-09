5fad494059374d832416556d3d882dbc71c31497
Danil <dinkodanil@gmail.com>
2021-03-09 11:28:12 +0300

    Connect to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import UserModule from './modules/user/user.module';
 
+
+const dbConnectURI =
+  'mongodb+srv://admin:admin@cluster0.yj5xp.mongodb.net/nextjs-learn?retryWrites=true&w=majority'
+
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(dbConnectURI)
+  ],
 })
 export class AppModule {}
