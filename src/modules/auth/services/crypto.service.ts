51a1245c4630d1700d024ceb1a9e15f90725313f
Danil <dinkodanil@gmail.com>
2021-03-10 08:09:17 +0300

    Start debuggind the crypto service

--- src/modules/auth/services/crypto.service.ts
+++ src/modules/auth/services/crypto.service.ts
@@ -5,31 +5,39 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export default class CryptoService {
-
   constructor(
     private readonly configService: ConfigService
-  ) {}
+  ) {
+  }
 
   encrypt(pass: string): string {
-    const cipher = crypto.createCipher(
-      this.configService.get<string>('CRYPT_ALGORITHM'),
-      this.configService.get<string>('CRYPT_SECRET_KEY')
-    )
-    let encrypted = cipher.update(pass,'utf8','hex')
-    encrypted += cipher.final('hex');
-    console.log(encrypted);
-    return encrypted
+    try {
+      const cipher = crypto.createCipher(
+        'aes-256-ctr',
+        'd6F3Efeq'
+      )
+      let encrypted = cipher.update(pass,'utf8','hex')
+      encrypted += cipher.final('hex');
+      console.log(encrypted);
+      return encrypted
+    } catch (e) {
+        console.log('Error in encrypting:', e)
+    }
   }
 
   decrypt(pass: string): string {
-    const decipher = crypto.createDecipher(
-      this.configService.get<string>('CRYPT_ALGORITHM'),
-      this.configService.get<string>('CRYPT_SECRET_KEY')
-    )
-    let decrypted = decipher.update(pass,'hex','utf8')
-    decrypted += decipher.final('utf8');
-    console.log(decrypted);
-    return decrypted
+    try {
+      const decipher = crypto.createDecipher(
+        'aes-256-ctr',
+        'd6F3Efeq'
+      )
+      let decrypted = decipher.update(pass,'hex','utf8')
+      decrypted += decipher.final('utf8');
+      console.log(decrypted);
+      return decrypted
+    } catch (e) {
+        console.log('Error in decrypting:', e)
+    }
   }
 
 }
